56f3199325678c96ea95376ab93ee567cb788815
joshnaacsha <joshnaacsha@gmail.com>
2025-02-21 17:55:22 +0530

    Changes made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All. This is test 5'));
+app.get('/', (req, res) => res.send('Hello All. Let us see if the pipeline works!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
